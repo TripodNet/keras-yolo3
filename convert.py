42a2e9e70d0d2ccb0f73cb5d31ae9a725296d440
Daniel Zhou <zhoudd0470@gmail.com>
2020-05-31 09:01:18 +0800

    switch to tf2.0 ver and convert.py ok

--- convert.py
+++ convert.py
@@ -11,15 +11,23 @@ import os
 from collections import defaultdict
 
 import numpy as np
-from keras import backend as K
-from keras.layers import (Conv2D, Input, ZeroPadding2D, Add,
-                          UpSampling2D, MaxPooling2D, Concatenate)
-from keras.layers.advanced_activations import LeakyReLU
-from keras.layers.normalization import BatchNormalization
-from keras.models import Model
-from keras.regularizers import l2
-from keras.utils.vis_utils import plot_model as plot
+# from keras import backend as K
+# from keras.layers import (Conv2D, Input, ZeroPadding2D, Add,
+#                           UpSampling2D, MaxPooling2D, Concatenate)
+# from keras.layers.advanced_activations import LeakyReLU
+# from keras.layers.normalization import BatchNormalization
+# from keras.models import Model
+# from keras.regularizers import l2
+# from keras.utils.vis_utils import plot_model as plot
 
+from tensorflow.keras import backend as K
+from tensorflow.keras.layers import (Conv2D, Input, ZeroPadding2D, Add,
+                          UpSampling2D, MaxPooling2D, Concatenate)
+from tensorflow.keras.layers import LeakyReLU
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.models import Model
+from tensorflow.keras.regularizers import l2
+from tensorflow.keras.utils import plot_model as plot
 
 parser = argparse.ArgumentParser(description='Darknet To Keras Converter.')
 parser.add_argument('config_path', help='Path to Darknet cfg file.')
